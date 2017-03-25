75557fda6e528cc52f876dcdb2ceee8a08197f6f
Ankit Jha <ankit-jha@users.noreply.github.com>
2017-03-24 23:10:30 -0400

    OSX Apps Installation Script

---
+++ osx_apps_install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Starting Apps Installation"
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+echo "Installing cask..."
+brew install caskroom/cask/brew-cask
+
+CASKS=(
+appcleaner
+boom
+caffeine
+calibre
+firefox
+flash-npapi
+google-chrome
+google-featured-photos
+handbrake
+opera
+skype
+skypewebplugin
+spotify
+torbrowser
+vagrant
+virtualbox
+vlc
+whatsapp
+xmind
+xquartz
+xquartz
+youtube-to-mp3
+zotero
+)
+
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+echo "Cleaning up..."
+brew cask cleanup
+
+echo "Apps installation complete"
