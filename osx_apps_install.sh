9d35c73276828cd685792c8b24b448dc21256c48
Ankit Jha <ankit-jha@users.noreply.github.com>
2017-09-12 21:08:28 -0400

    Update osx_apps_install.sh

--- osx_apps_install.sh
+++ osx_apps_install.sh
@@ -47,5 +47,5 @@ echo "Cleaning up..."
 brew cask cleanup
 
 echo "Apps installation complete"
-export PS1="\[\e[32;1m\]\u@\[\e[32;1m\]\h: \[\e[34;1m\]\W \[\e[0m\]\$"
+export PS1="\[\e[32;1m\]\u@\[\e[32;1m\]\h: \[\e[34;1m\]\W \[\e[0m\]\$ "
 echo "Prompt Update"
