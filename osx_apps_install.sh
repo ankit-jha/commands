3b63ee9794d3b4e981d31c4c310b9ed0453b1691
ankit-jha <6148327+ankit-jha@users.noreply.github.com>
2022-03-11 14:05:19 -0600

    Update brew install script

--- osx_apps_install.sh
+++ osx_apps_install.sh
@@ -5,7 +5,7 @@ echo "Starting Homebrew Installation"
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "Starting Core Apps Installation"
